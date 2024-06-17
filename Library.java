72e00567abde1547f865981a81a0e867dfbacac5
david30-maker <davidokpah396@gmail.com>
2024-06-17 22:08:30 +0100

    create library class

---
+++ Library.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.ArraytList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+}
