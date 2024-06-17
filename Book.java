c890e08bde5ca9fdcfbb46db71ba599c1e1ee3d3
david30-maker <davidokpah396@gmail.com>
2024-06-17 22:03:29 +0100

    add book attributes to book class

--- Book.java
+++ Book.java
@@ -2,4 +2,28 @@ public class  Book {
     private String title;
     private String author;
     private String datePublished;
+
+    public Book(String title, String author, String datePublished) {
+        this.title = title;
+        this.author = author;
+        this.datePublished = datePublished;
+    }
+
+    public String geTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDatePublished() {
+        return datePublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author +
+        ", Published: " + datePublished;
+    }
 }
