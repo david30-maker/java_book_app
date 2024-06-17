2ffd29bf5feed3a559bbb9b536411ac0e36f330c
david30-maker <davidokpah396@gmail.com>
2024-06-17 22:21:11 +0100

    add addbook method and removebook method to library class

--- Library.java
+++ Library.java
@@ -6,8 +6,28 @@ import java.util.List;
 
 public class Library {
     private List<Book> books;
+    private static final String FILENAME= "library.txt";
+
 
     public Library() {
         this.books = new ArrayList<>();
+        loadBooks();
+    }
+
+    public void addBook(Book book) {
+        books.add(book)
+        saveBooks();
+    }
+
+    public void removeBook(String title) {
+        Iterator<Book> iterator = books.iterator();
+        while (iterator.hasNext()) {
+            Book book = iterator.next();
+            if (book.getTitle().equals(title)) {
+                iterator.remove();
+                System.out.println("Book removed:" + book.getTitle());
+                saveBooks();
+            }
+        }
     }
 }
